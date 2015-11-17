3f4ac2c954bea299136fd72830a8445562bf16d7
adncha <adnane.chaabi@myseat.fr>
2015-11-16 20:40:27 -0500

    minor changes

--- wp-content/themes/learningWordPress/header.php
+++ wp-content/themes/learningWordPress/header.php
@@ -9,34 +9,35 @@
 
 <body <?php body_class(); ?>>
 
-	<div class="container">
+	<!-- site-header -->
+	<header class="site-header">
 
-		<!-- site-header -->
-		<header class="site-header">
-
-			<!-- hd-search -->
-			<!--<div class="hd-search">
+		<!-- hd-search -->
+		<!--<div class="hd-search">
 				<?php get_search_form(); ?>
 			</div>--><!-- /hd-search -->
-
+		<div class="mtu-header">
 			<h1 class="mtu-title"><a href="<?php echo home_url(); ?>"> <?php bloginfo('name'); ?></a></h1>
 			<h5><?php bloginfo('description'); ?> <?php if (is_page('portfolio')) { ?>
-				- Thank you for viewing our work
-			<?php }?></h5>
+					- Thank you for viewing our work
+				<?php }?></h5>
+		</div>
 
 
 
-			<nav class="site-nav">
+		<nav class="site-nav">
 
-				<?php
+			<?php
 
 				$args = array(
-					'theme_location' => 'primary'
+						'theme_location' => 'primary'
 				);
 
-				?>
+			?>
 
-				<?php wp_nav_menu(  $args ); ?>
-			</nav>
+			<?php wp_nav_menu(  $args ); ?>
+		</nav>
 
-		</header><!-- /site-header -->
+	</header><!-- /site-header -->
+
+	<div class="container">
